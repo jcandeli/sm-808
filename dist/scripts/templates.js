fe380c3bac9a6d1bb5d633167871045a1bdebffe
JP Candelier <jp.candelier@zefr.com>
2015-01-19 21:27:24 -0800

    functioning sequencer + styling

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -2,24 +2,52 @@ angular.module('sm808App').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('modules/sm-808/sequencerView.html',
-    "<div class=\"sequencer\">\n" +
-    "    <div ng-repeat=\"beat in beats\">\n" +
+    "<div>\n" +
+    "\n" +
+    "    <div class=\"sequencer\">\n" +
+    "        <h1>SM-808</h1>\n" +
+    "\n" +
+    "        <div class=\"info\">\n" +
+    "            <h3>{{beat.name}}</h3>\n" +
+    "            <h4>BPM {{beat.bpm}}</h4>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <div class=\"padded-lg bordered\">\n" +
+    "            <ul class=\"list-unstyled\">\n" +
+    "                <li ng-repeat=\"(sample, sequence) in beat.sequence\">\n" +
+    "\n" +
+    "                    <div class=\"sample\">{{sample}}</div>\n" +
+    "\n" +
+    "                    <ul class=\"list-inline notes\">\n" +
+    "                        <li\n" +
+    "                            ng-repeat=\"note in sequence track by $index\"\n" +
+    "                            class=\"note\"\n" +
+    "                            ng-class=\"{selected: note, playing: $index == step}\"\n" +
+    "                            ng-click=\"toggleNote(sequence, $index)\"\n" +
+    "                        ></li>\n" +
+    "                    </ul>\n" +
+    "\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "\n" +
+    "        <a ng-click=\"toggleState()\" class=\"btn btn-primary\">\n" +
+    "            <i class=\"glyphicon\" ng-class=\"{'glyphicon-play': state == 'stopped', 'glyphicon-stop': state == 'playing'}\"></i>\n" +
+    "            <span ng-if=\"state == 'stopped'\">Start</span>\n" +
+    "            <span ng-if=\"state == 'playing'\">Stop</span>\n" +
+    "        </a>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"margined\">\n" +
+    "        <h4>Beats</h4>\n" +
     "\n" +
     "        <ul class=\"list-unstyled\">\n" +
-    "            <li ng-repeat=\"(sample, sequence) in beat.sequence\">\n" +
-    "\n" +
-    "                <div class=\"sample\">{{sample}}</div>\n" +
-    "\n" +
-    "                <ul class=\"list-inline notes\">\n" +
-    "                    <li ng-repeat=\"note in sequence track by $index\" class=\"note\">\n" +
-    "                        {{note}}\n" +
-    "                    </li>\n" +
-    "                </ul>\n" +
-    "\n" +
+    "            <li ng-repeat=\"beat in beats\">\n" +
+    "                <a ng-click=\"selectBeat(beat)\">{{beat.name}}</a>\n" +
     "            </li>\n" +
     "        </ul>\n" +
-    "\n" +
     "    </div>\n" +
+    "\n" +
     "</div>"
   );
 
--- src/modules/sm-808/sequencerCtr.coffee
+++ src/modules/sm-808/sequencerCtr.coffee
@@ -5,8 +5,49 @@ utils.addDirective('sequencer', () ->
     replace: true
     templateUrl: 'modules/sm-808/sequencerView.html'
 
-    controller: ['$scope', 'beatService', ($scope, beatService) ->
+    controller: ['$scope', 'beatService', '$interval', ($scope, beatService, $interval) ->
+        # =========
+        # Init
+        # =========
+        task = null
+
+        # get all the saved beats
         beatService.getBeats().then (beats) ->
             $scope.beats = beats.data
+            $scope.beat  = $scope.beats[0] # select first beat by default
+
+        # =========
+        # private functions
+        # =========
+        calcBeatInterval = (bpm) ->
+            (60/bpm * 1000) / 4
+
+        nextStep = () ->
+            # step through each subdivision 
+            $scope.step = if $scope.step == 15 then 0 else $scope.step + 1
+
+
+        # =========
+        # scoped variables
+        # =========
+        $scope.state = 'stopped'
+        $scope.step  = -1
+
+        $scope.toggleState = () ->
+            if $scope.state == 'stopped'
+                $scope.state = 'playing'
+                task = $interval(nextStep, calcBeatInterval($scope.beat.bpm))
+
+            else
+                $interval.cancel(task)
+                $scope.step = -1
+                $scope.state = 'stopped'
+
+        $scope.selectBeat = (beat) ->
+            $scope.beat = beat
+
+        $scope.toggleNote = (sequence, index) ->
+            sequence[index] = if sequence[index] then '' else 'x'
+
     ]
 )
